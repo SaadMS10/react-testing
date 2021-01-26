10b797345c4bc1ba3b0f37d2587788f4e06c6222
Muhammad Saad <itsmsaadhere@gmail.com>
2021-01-26 20:53:11 +0500

    React Test Cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('render hello world', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('find world in the document', () => {
+  const renderResponse = render(<App />);
+
+  const worldElement = renderResponse.getByText(/User/i);
+  expect(worldElement).toBeInTheDocument();
+});
+
+test('find input with placeholder', () => {
+  const renderResponse = render(<App />);
+  const worldElement = renderResponse.getByPlaceholderText(/name here/i);
+  expect(worldElement).toBeInTheDocument();
 });
